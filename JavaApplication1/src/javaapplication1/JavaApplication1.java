3937f53b132858f098db99b794f5be0958fb20f1
NathanMaced <nathanmacedo02@gmail.com>
2022-11-22 19:27:50 -0300

    MAtriz

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,7 +3,12 @@ package javaapplication1;
 public class JavaApplication1 {
 
 public static void main(String[] args) {
-        
+            int [][] vet = new int[3][3];
+            int l, c;
+            for (l=0; 1<3; l++){
+            for (c=0; 1<3; c++){
+                
+            }}
     }
     
 }
