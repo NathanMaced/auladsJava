371938df39f73bb490125a75fa1fcfc2934845b4
NathanMaced <nathanmacedo02@gmail.com>
2022-11-22 19:44:19 -0300

    Caça Palavras

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -1,14 +1,25 @@
 package javaapplication1;
 
+import javax.swing.JOptionPane;
+
 public class JavaApplication1 {
 
 public static void main(String[] args) {
-            int [][] vet = new int[3][3];
+            int [][] valor = new int[3][3];
             int l, c;
+            String total = " ";
+            
+            
             for (l=0; 1<3; l++){
-            for (c=0; 1<3; c++){
-                
-            }}
+                for (c=0; 1<3; c++){
+                String x = JOptionPane.showInputDialog(null, "Digite o valor da linha "+1+" e coluna "+c);
+                valor [l][c] = Integer.parseInt(x);
+                total = total + valor [l][c]+" ";
+            }
+            total = total + "\n";
+            }
+            JOptionPane.showMessageDialog(null, total);
+            
     }
     
 }
---
+++ caca_palavras/src/caca_palavras/Caca_palavras.java
@@ -0,0 +1,7 @@
+package caca_palavras;
+public class Caca_palavras {
+    public static void main(String[] args) {
+        
+    }
+    
+}
